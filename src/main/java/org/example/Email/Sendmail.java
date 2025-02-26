a460b4e08a8107a6efb4417ad6b2dabdb8ae993e
aziz <siliconoxide4@gmail.com>
2025-02-26 08:05:09 +0600

    ip address added

--- src/main/java/org/example/Email/Sendmail.java
+++ src/main/java/org/example/Email/Sendmail.java
@@ -6,14 +6,82 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.Socket;
+import java.net.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Enumeration;
+import java.util.List;
 import java.util.Scanner;
 
 public class Sendmail {
 
     public static void main(String[] args) {
+
+    getIpAddress();
+
+    }
+
+    public  static void getIpAddress(){
+        try {
+            /**
+             * Get All Network Interface
+             */
+            Enumeration<NetworkInterface> networkInterface= NetworkInterface.getNetworkInterfaces();
+           while (networkInterface.hasMoreElements()){
+            NetworkInterface network=networkInterface.nextElement();
+               System.out.println("Interface Name:"+network.getName());
+               if(network.getName().equals("wlp0s20f3")){
+                  byte[] hardwareAddress=  network.getHardwareAddress();
+                  StringBuilder mac=new StringBuilder();
+                  for (byte b:hardwareAddress){
+                      mac.append(String.format("%02X:",b));
+                  }
+                  System.out.println("Mac Address: "+mac);
+                  Enumeration<InetAddress> address=network.getInetAddresses();
+                   while (address.hasMoreElements()){
+                       InetAddress add=address.nextElement();
+                        if(add instanceof Inet4Address){
+                            System.out.println("IPv4:"+add.getHostAddress());
+                        }
+                        else if(add instanceof Inet6Address){
+                            System.out.println("IPv6:"+add.getHostAddress());
+                        }
+
+                   }
+               }
+
+               else if (network.getName().equals("docker0")){
+                   System.out.println("DisplayName "+network.getDisplayName());
+                   byte[] hardwareAddress=network.getHardwareAddress();
+                   StringBuilder macAdd=new StringBuilder();
+                   for(byte b:hardwareAddress){
+                       macAdd.append(String.format("%02X:",b));
+                   }
+                   System.out.println(network.getName()+" MAC: "+macAdd);
+                   Enumeration<InetAddress> addressEnumeration=network.getInetAddresses();
+                   while (addressEnumeration.hasMoreElements()){
+                       InetAddress add=addressEnumeration.nextElement();
+                       if(add instanceof Inet4Address){
+                           System.out.println("IPv4:"+add.getHostAddress());
+                       }
+                       else if(add instanceof Inet6Address){
+                           System.out.println("IPv6:"+add.getHostAddress());
+                       }
+
+                   }
+
+               }
+           }
+
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    public static  void  sendMail(){
 //        Scanner scanner=new Scanner(System.in);
         String smtpServer="smtp.gmail.com";
         int smtpPort=465;
@@ -65,10 +133,7 @@ public class Sendmail {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-
     }
-
     public static void printServerResponse(BufferedReader reader){
        try {
            System.out.println("Server:"+reader.readLine());
